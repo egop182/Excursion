b11db6d90abf496d5fbe0d0069edbcf870cf428e
Edwin Ospina <gy0v4nny182@gmail.com>
2018-08-17 15:28:28 -0500

    Creando el proyecto en repositorio

---
+++ Excursion/Elementos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excursion
+{
+    public partial class Elementos : Form
+    {
+        public Elementos()
+        {
+            InitializeComponent();
+        }
+    }
+}
